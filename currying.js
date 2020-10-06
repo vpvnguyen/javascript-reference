2200e86033298224c7259f0ce379cbcd5fc1fd07
Vincent Nguyen <vpvnguyen@gmail.com>
2020-10-05 18:48:03 -0700

    cleaned up example

--- currying.js
+++ currying.js
@@ -1,59 +1,10 @@
-const str1 = "Hello";
-const str2 = "World";
-const greeting = "greeting";
-const separator = "separator";
-const emphasis = "emphasis";
-const name = "vincent";
-
-const greet = str1 => (greet2 = str2 => `${str1}, ${str2}!`);
-
-console.log(greet(str1)(str2));
+const greet = (str1) => (greet2 = (str2) => `${str1}, ${str2}!`);
 
 // https://www.sitepoint.com/currying-in-functional-javascript/
-var greetCurried = function(greeting) {
-  return function(name) {
-    console.log(greeting + ", " + name);
-  };
-};
+const car = (manufacturer) => (year) => (model) =>
+  console.log(manufacturer, year, model);
 
-var greetHello = greetCurried("Hello");
-
-greetHello("Heidi");
-greetHello("Eddie");
-greetCurried("Hi there")("Howard");
-
-var greetDeeplyCurried = function(greeting) {
-  console.log(1);
-  return function(separator) {
-    console.log(2);
-    return function(emphasis) {
-      console.log(3);
-      return function(name) {
-        console.log(4);
-        console.log(`${greeting} ${separator} ${name} ${emphasis}`);
-      };
-    };
-  };
-};
-
-greetDeeplyCurried(greeting)(separator)(emphasis)(name);
-
-
-var greetDeeplyCurried2 = function(greeting) {
-    return function(separator) {
-      return function(emphasis) {
-        return function(name) {
-          console.log(greeting + separator + name + emphasis);
-        };
-      };
-    };
-  };
-  
-  var greetAwkwardly = greetDeeplyCurried2("Hello")("...")("?");
-  greetAwkwardly("Heidi");
-  
-  var sayHello = greetDeeplyCurried2("Hello")(", ");
-  sayHello(".")("Heidi");
-  
-  var askHello = sayHello("?");
-  askHello("Heidi");
+car("nissan")("1987")("240sx");
+car("nissan")("1987")("");
+car("nissan")("")("");
+car("nissan")(null)("240sx");
