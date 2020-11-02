9554c7c7534e8a7fed066236d2dad1726c140e82
Vincent Nguyen <vpvnguyen@gmail.com>
2020-11-01 20:13:21 -0800

    append api key to url directly

--- fs/get-request.js
+++ fs/get-request.js
@@ -2,22 +2,17 @@ require("dotenv").config();
 const axios = require("axios");
 const fs = require("fs");
 
-const openWeatherUrl = (apiKey) =>
-  `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
+const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${process.env.OPEN_WEATHER_API_KEY}`;
 
 const getRequest = async () => {
-  const openWeatherRequestUrl = openWeatherUrl(
-    process.env.OPEN_WEATHER_API_KEY
-  );
-
-  const weatherData = await axios.get(openWeatherRequestUrl);
+  const weatherData = await axios.get(openWeatherUrl);
   return weatherData.data;
 };
 
 const writeToFile = (data) => {
   const filename = `openweather.json`;
   fs.writeFile(filename, data, function (err) {
-    if (err) return console.log(err);
+    if (err) return console.error(err);
     console.log(`Wrote to file: ${filename}`);
   });
 };
