d036fe2badc82be96f8d6c2745a1cb41798ab99a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-11-01 20:14:05 -0800

    modify openweather's name and create a new file

--- fs/modify-file.js
+++ fs/modify-file.js
@@ -2,5 +2,17 @@ const fs = require("fs");
 
 const weatherData = fs.readFileSync("openweather.json");
 const parsedData = JSON.parse(weatherData);
+parsedData.name = "New Name";
+
+const writeToFile = (data) => {
+  const filename = `openweather_modified.json`;
+  fs.writeFile(filename, data, function (err) {
+    if (err) return console.error(err);
+    console.log(`Wrote to file: ${filename}`);
+  });
+};
+
+writeToFile(JSON.stringify(parsedData));
+
 console.log(parsedData);
-console.log(parsedData.coord);
+console.log(parsedData.name);
