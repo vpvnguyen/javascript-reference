3ee68e144e139f1b3d7d84e6e57aa9e3461c711f
vpvnguyen <vpvnguyen@gmail.com>
2021-04-24 13:47:32 -0700

    create array method logging utility

---
+++ methods/server/utilities/LogOperations.ts
@@ -0,0 +1,36 @@
+export default class LogOperations {
+  private readonly logOperationsResponse: {
+    operation: string;
+    arrayBefore: any[];
+    arrayAfter: any[] | null | undefined;
+    mutates: boolean;
+    result: any;
+  };
+
+  constructor(operationName: string, array: any[], callback: any) {
+    const input: any = [...array];
+    const result: any = callback(array);
+
+    this.logOperationsResponse = {
+      operation: operationName,
+      arrayBefore: input,
+      arrayAfter: array,
+      mutates: this.mutatesArray(input, array), // shallow check
+      result,
+    };
+  }
+
+  public getResponse = () => this.logOperationsResponse;
+
+  private mutatesArray = (input: any, array: any) => {
+    if (!input || !array) return true;
+    if (input !== array) return true;
+    if (input.length !== array.length) return true;
+
+    input.forEach((item: any, index: number) => {
+      if (item !== array[index]) return true;
+    });
+
+    return false;
+  };
+}
