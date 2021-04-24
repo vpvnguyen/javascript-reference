08b4e203d6e770b126eefa2af6de91e47beec789
vpvnguyen <vpvnguyen@gmail.com>
2021-04-24 14:32:03 -0700

    create forEach method

---
+++ methods/server/classes/Iteration.ts
@@ -0,0 +1,10 @@
+export default class Iteration {
+  public static forEach = (array: any, callback: any): void => {
+    const { length } = array;
+
+    for (let index = 0; index < length; index += 1) {
+      const value = array[index];
+      callback(value, index, array);
+    }
+  };
+}
