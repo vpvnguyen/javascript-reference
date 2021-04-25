bc63889d99f7a1f172a2bc65dc5bb38c1d9e1879
vpvnguyen <vpvnguyen@gmail.com>
2021-04-24 23:09:43 -0700

    defined iteration routes instance; export iteration routes

---
+++ methods/server/routes/index.ts
@@ -0,0 +1,9 @@
+import iterationsRouter from "./iterations.router";
+
+const iterationsRoutes = {
+  name: "Iterations Routes",
+  path: "/iterations",
+  router: iterationsRouter,
+};
+
+export { iterationsRoutes };
