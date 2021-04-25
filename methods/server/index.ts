5797c378556ead1ec1d5da64675a95251bf7a73c
vpvnguyen <vpvnguyen@gmail.com>
2021-04-24 23:12:30 -0700

    use iteration routes; defined domain url of local server

--- methods/server/index.ts
+++ methods/server/index.ts
@@ -1,28 +1,15 @@
 import express, { Express, Request, Response } from "express";
-import LogOperations from "./utilities/LogOperations";
-import Iteration from "./classes/Iteration";
+import { iterationsRoutes } from "./routes";
 
 const app: Express = express();
 const PORT: number = 5000;
 
-const numsArray: number[] = [1, 2, 3, 4, 5];
-
-app.get("/iteration/forEach", (req: Request, res: Response) => {
-  const operationName: string = "forEach";
-  try {
-    const log = new LogOperations(operationName, numsArray, (array: any) =>
-      Iteration.forEach(array, (value: any) => value)
-    );
-    const response = log.getResponse();
-    res.json(response);
-  } catch (error) {
-    console.error(operationName, error);
-    res.json(error);
-  }
-});
+app.use(iterationsRoutes.path, iterationsRoutes.router);
 
 app.get("/", (req: Request, res: Response) => {
   res.json(`HOME`);
 });
 
-app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
