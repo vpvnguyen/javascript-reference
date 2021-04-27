83a3abe8c3c732eecbf43724761aeb922b8bf651
vpvnguyen <vpvnguyen@gmail.com>
2021-04-27 15:46:26 -0700

    remove identify array method middleware from parent route

--- methods/server/api/index.ts
+++ methods/server/api/index.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { identifyArrayMethod } from "../middleware";
 import arrayMethodsRouter from "./arrayMethods";
 
 const router: Router = Router();
 
-router.use("/arrayMethods", identifyArrayMethod, arrayMethodsRouter);
+router.use("/arrayMethods", arrayMethodsRouter);
 
 router.get("/", (req, res) => {
   res.json("API");
