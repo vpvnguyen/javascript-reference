878fffc62bb37280f5a1a06f81b651e4e5668cc0
vpvnguyen <vpvnguyen@gmail.com>
2021-04-27 15:44:49 -0700

    pass in method name, input array and callback to get log operation's response

--- methods/server/api/arrayMethods/ArrayMethods.controller.ts
+++ methods/server/api/arrayMethods/ArrayMethods.controller.ts
@@ -1,15 +1,16 @@
 import { LogOperationsService } from "../../services";
-const numsArray = [1, 2, 3, 4, 5];
+
+const numsArray: number[] = [1, 2, 3, 4, 5];
+
 export default class ArrayMethodsController {
   private response: any;
   constructor(name: string, callback: any) {
     try {
       const log = new LogOperationsService(name, numsArray, callback);
       const response = log.getResponse();
-      console.log(response);
       this.response = response;
     } catch (error) {
-      console.error(name, error);
+      console.error("ArrayMethodsController Error", name, error);
       this.response = error;
     }
   }
