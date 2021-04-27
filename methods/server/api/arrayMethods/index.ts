5f5d6f00d258e2746de6d789d230908d906a84e0
vpvnguyen <vpvnguyen@gmail.com>
2021-04-27 15:46:04 -0700

    create dynamic req param to find method; use identify array method middleware to select array method to be ran

--- methods/server/api/arrayMethods/index.ts
+++ methods/server/api/arrayMethods/index.ts
@@ -1,20 +1,25 @@
 import { Router, Request, Response } from "express";
 import ArrayMethodsController from "./ArrayMethods.controller";
+import { identifyArrayMethodParams } from "../../middleware";
 
 const router: Router = Router();
 
-router.get("/:method", (req: Request, res: Response) => {
-  try {
-    const arrayMethodsController = new ArrayMethodsController(
-      res.locals.arrayMethod.name,
-      res.locals.arrayMethod.callback
-    );
-    const response = arrayMethodsController.getResponse();
-    res.json(response);
-  } catch (error) {
-    console.error("arraymethodscontroller", error);
-    res.json("error");
+router.get(
+  "/:method",
+  identifyArrayMethodParams,
+  (req: Request, res: Response) => {
+    try {
+      const arrayMethodsController = new ArrayMethodsController(
+        res.locals.arrayMethod.name,
+        res.locals.arrayMethod.callback
+      );
+      const response = arrayMethodsController.getResponse();
+      res.json(response);
+    } catch (error) {
+      console.error("arraymethodscontroller", error);
+      res.json("error");
+    }
   }
-});
+);
 
 export default router;
