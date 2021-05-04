3e724be246b198d75adc08ab7b8fbee1a9ca1500
Vincent Nguyen <vpvnguyen@gmail.com>
2021-05-03 20:00:00 -0600

    create findIndex under the hood

---
+++ methods/findIndex/findIndex-under-the-hood.js
@@ -0,0 +1,36 @@
+function findIndex(array, callback) {
+  const { length } = array;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+
+    if (callback(value, index, array)) {
+      return index;
+    }
+  }
+
+  return -1;
+}
+
+function logOperation(operationName, array, callback) {
+  const input = [...array];
+  const result = callback(array);
+
+  console.log({
+    operation: operationName,
+    arrayBefore: input,
+    arrayAfter: array,
+    result,
+  });
+}
+
+logOperation("findIndex", [1, 2, 3, 4, 5], (array) =>
+  findIndex(array, (number) => number === 3)
+);
+
+// {
+//     operation: 'findIndex',
+//     arrayBefore: [ 1, 2, 3, 4, 5 ],
+//     arrayAfter: [ 1, 2, 3, 4, 5 ],
+//     result: 2
+// }
