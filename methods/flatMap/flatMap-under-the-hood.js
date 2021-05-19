7b66406c9844ab58560b5d1165bd89fe6c11f9fe
Vincent Nguyen <vpvnguyen@gmail.com>
2021-05-18 20:00:38 -0700

    create flatMap under the hood

---
+++ methods/flatMap/flatMap-under-the-hood.js
@@ -0,0 +1,100 @@
+function map(array, callback) {
+  const result = [];
+  const { length } = array;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = array[index];
+
+    result[index] = callback(value, index, array);
+  }
+
+  return result;
+}
+
+function flat(array, depth = 0) {
+  if (depth < 1 || !Array.isArray(array)) {
+    return array;
+  }
+
+  return reduce(
+    array,
+    (result, current) => {
+      return concat(result, flat(current, depth - 1));
+    },
+    []
+  );
+}
+
+function reduce(array, callback, initValue) {
+  const { length } = array;
+
+  let acc = initValue;
+  let startAtIndex = 0;
+
+  if (initValue === undefined) {
+    acc = array[0];
+    startAtIndex = 1;
+  }
+
+  for (let index = startAtIndex; index < length; index += 1) {
+    const value = array[index];
+    acc = callback(acc, value, index, array);
+  }
+
+  return acc;
+}
+
+function concat(array, ...values) {
+  const result = [...array];
+  const { length } = values;
+
+  for (let index = 0; index < length; index += 1) {
+    const value = values[index];
+
+    if (Array.isArray(value)) {
+      push(result, ...value);
+    } else {
+      push(result, value);
+    }
+  }
+
+  return result;
+}
+
+function push(array, ...values) {
+  const { length: arrayLength } = array;
+  const { length: valuesLength } = values;
+
+  for (let index = 0; index < valuesLength; index += 1) {
+    array[arrayLength + index] = values[index];
+  }
+
+  return array.length;
+}
+
+function flatMap(array, callback) {
+  return flat(map(array, callback), 1);
+}
+
+function logOperation(operationName, array, callback) {
+  const input = [...array];
+  const result = callback(array);
+
+  console.log({
+    operation: operationName,
+    arrayBefore: input,
+    arrayAfter: array,
+    result,
+  });
+}
+
+logOperation("flatMap", [1, 2, 3], (array) =>
+  flatMap(array, (number) => [number, number])
+);
+
+// {
+//     operation: 'flatMap',
+//     arrayBefore: [ 1, 2, 3 ],
+//     arrayAfter: [ 1, 2, 3 ],
+//     result: [ 1, 1, 2, 2, 3, 3 ]
+// }
